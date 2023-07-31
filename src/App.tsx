9f7ad6d63973eec79bed3af63dc638f9369aa14a
joryober <33765207+joryober@users.noreply.github.com>
2023-07-31 07:16:36 -0700

    set up input states

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,26 @@ import './App.css'
 
 function App() {
 
+  const [coinX, setCoinX] = useState("")
+  const [coinY, setCoinY] = useState("")
+
+  const handleChangeX = (event) => {
+    setCoinX(event.target.value)
+  }
+
+  const handleChangeY = (event) => {
+    setCoinY(event.target.value)
+  }
+
+  const handleClick = () => {
+    console.log(`${coinX}, ${coinY}`)
+  }
+
   return (
     <>
       <h1>X Priced in Y</h1>
-      <p>I want <input type="text" /> priced in <input type="text" /></p>
-      <button>Go!</button>
+      <p>I want <input type="text" value={coinX} onChange={handleChangeX}/> priced in <input type="text" value={coinY} onChange={handleChangeY}/></p>
+      <button onClick={handleClick}>Go!</button>
     </>
   )
 }
