81344fc9d975a021a90bfc1d2bb249950313c3e7
joryober <33765207+joryober@users.noreply.github.com>
2023-08-01 07:31:31 -0700

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -58,13 +58,14 @@ function App() {
   return (
     <>
       <h1>X Priced in Y</h1>
-      <div className="flex">
+      <div className="flex py-5 items-center justify-between">
         <p>I want <input type="text" value={coinX} onChange={handleChangeX}/> priced in <input type="text" value={coinY} onChange={handleChangeY}/></p>
         <button onClick={handleClick}>Go!</button>
       </div>
       {coinXPrice && coinYPrice && 
-      <h3><input type='text' value={amounts.x} onChange={handleChangeXAmount}/>{`${displayCoinX} is equal to `}<input type='text' value={amounts.y} onChange={handleChangeYAmount}/>{` ${displayCoinY}`}</h3>
+      <h3><input className="text-right h-8" type='text' value={amounts.x} onChange={handleChangeXAmount}/>{` ${displayCoinX} is equal to `}<input className="text-right h-8" type='text' value={amounts.y} onChange={handleChangeYAmount}/>{` ${displayCoinY}`}</h3>
       }
+      <footer className="h-8 fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600 text-gray-500">Powered by Coingecko API</footer>
     </>
   )
 }
